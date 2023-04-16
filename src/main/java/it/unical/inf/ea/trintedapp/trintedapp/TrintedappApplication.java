a53734853f14df2cbdf6f7514d214a513910f958
pit0500 <pietro.macri2000@gmail.com>
2023-04-16 12:38:55 +0200

    backend: Initial commit

---
+++ src/main/java/it/unical/inf/ea/trintedapp/trintedapp/TrintedappApplication.java
@@ -0,0 +1,13 @@
+package it.unical.inf.ea.trintedapp.trintedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrintedappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrintedappApplication.class, args);
+	}
+
+}
