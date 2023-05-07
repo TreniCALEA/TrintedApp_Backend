d5800b06ab4736980803d5ff2e6c4b9387e32ac1
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2023-05-07 11:15:51 +0200

    backend: OrdineDto: add class annotations

--- src/main/java/it/unical/inf/ea/trintedapp/dto/OrdineDto.java
+++ src/main/java/it/unical/inf/ea/trintedapp/dto/OrdineDto.java
@@ -4,7 +4,13 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@NoArgsConstructor
+@ToString
 public class OrdineDto {
     
     private Long id;
